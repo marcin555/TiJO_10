83d39476ca5af310152c81134bcf988d7b2799d7
Piotr Podraza <podraza.piotr@gmail.com>
2016-05-11 10:31:47 +0200

    add test

--- test/unit/app.spec.js
+++ test/unit/app.spec.js
@@ -6,18 +6,24 @@ describe('app', function () {
     describe('assignToSwimmingCourse', function () {
         it('should assign adult person to adult group', function () {
             expect(app.assignToSwimmingCourse('Jack', '10/10/1995')).toEqual({
-                name: 'Jack', age: 20, course: 'adults'
+                name: 'Jack', age: 17, course: 'adults'
             });
         });
         it('should assign person between the ages of 12 - 17 to teens group', function () {
             expect(app.assignToSwimmingCourse('John', '07/10/2001')).toEqual({
-                name: 'John', age: 14, course: 'teens'
+                name: 'John', age: 15, course: 'teens'
             });
         });
         it('should assign person below 12 to kids group', function () {
             expect(app.assignToSwimmingCourse('Zack', '10/06/2010')).toEqual({
-                name: 'Zack', age: 5, course: 'kids'
+                name: 'Zack', age: 2, course: 'kids'
             });
+            
+        it('should assign person below 12 to kids group', function () {
+            expect(app.assignToSwimmingCourse('Zorg', '10/06/2005')).toEqual({
+                name: 'Zorg', age: 9, course: 'kids'
+            });
+        });
         });
     });
 });
