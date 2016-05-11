ecea03e5c59725bd5ca24c9802ca1502d237feb7
Piotr Podraza <podraza.piotr@gmail.com>
2016-05-11 10:39:46 +0200

    add test

--- test/unit/app.spec.js
+++ test/unit/app.spec.js
@@ -18,14 +18,33 @@ describe('app', function () {
             expect(app.assignToSwimmingCourse('Zack', '10/06/2010')).toEqual({
                 name: 'Zack', age: 2, course: 'kids'
             });
-            
+
         it('should assign person below 12 to kids group', function () {
-            expect(app.assignToSwimmingCourse('Zorg', '10/06/2005')).toEqual({
-                name: 'Zorg', age: 9, course: 'kids'
+            expect(app.assignToSwimmingCourse('Adam', '10/06/2005')).toEqual({
+                name: 'Adam', age: 9, course: 'kids'
             });
         });
         });
     });
 });
 
+describe('calculateArea', function () {
+    it('Should return the area of figure and message', function () {
+        expect(answer.calculateArea(21, 2, 0, 'Success', 'Fail')).toEqual({area: 21, message: 'Success'});
+        expect(answer.calculateArea(21, 8, 6, 'Success', 'Fail')).toEqual({area: -27, message: 'Fail'});
+        expect(answer.calculateArea(34, 2, 0, 'Success', 'Fail')).toEqual({area: 34, message: 'Success'});
+        expect(answer.calculateArea(21, 'a', 0, 'Success', 'Fail')).toEqual(false);
 
+    });
+
+    it('Should return false', function () {
+        expect(answer.calculateArea('aaaaaaa', 2, 0, 'Success', 'Fail')).toEqual(false);
+        expect(answer.calculateArea(67, 8, 6, 3, 5)).toEqual(false);
+        expect(answer.calculateArea(34, 2, 'b', 5, 'Fail')).toEqual(false);
+        expect(answer.calculateArea(21, 'a', 0, 'Success', 'Fail')).toEqual(false);
+    });
+
+    it('Should return area 0 and message', function () {
+        expect(answer.calculateArea(0, 0, 0, 'Success', 'Fail')).toEqual({area: 0, message: 'Big null'});
+    });
+});
